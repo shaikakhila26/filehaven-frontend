03f2892c5b84fbe9f3deab2bbf59a7d8b22a2e7e
shaikakhila26 <akhilashaik2605@gmail.com>
2025-08-21 17:47:07 +0530

    solving folder upload error in my drive only 1

--- src/components/modals/FolderUploadModal.jsx
+++ src/components/modals/FolderUploadModal.jsx
@@ -24,7 +24,7 @@ const FolderUploadModal = ({ onClose, onSuccess ,folderId}) => {
 if (folderId && folderId !== "root" && folderId !== "null") {
   formData.append("folder_id", folderId);
 } else {
-  formData.append("folder_id", ""); // <-- ensure backend receives something
+  formData.append("folder_id", null); // <-- ensure backend receives something
 }
 
 
